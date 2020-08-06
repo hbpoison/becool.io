c537350b4b55288da4e3b3806057e4dd2a2009e5
Paul Qiu <hbpoison@gmail.com>
2020-08-06 22:04:18 +0900

    new sit

--- f.js
+++ f.js
@@ -7,24 +7,27 @@ if (window.location.pathname.includes('default.aspx')) {
   }
 }
 
-var trans = [
-  ' <tr class="odd gradeX"><td>2020/8/6 5:30:51</td><td class="center">28500.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">28500.00</td><td>农业银行</td><td>陈文辉</td><td>6230520690014993576</td><td>123456</td> <td>已完成</td> </tr></tr>',
-  '<tr class="odd gradeX"><td>2020/8/6 7:39:43</td><td class="center">39916.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39916.00</td><td>农业银行</td><td>宋玉芝</td><td>6228481699445512179</td><td>123456</td> <td>已完成</td> </tr>   </tr>',
-  '<tr class="odd gradeX"><td>2020/8/6 7:40:27</td><td class="center">40108.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">40108.00</td><td>工商银行</td><td>吴夏星</td><td>6212261408030687291</td><td>123456</td><td>已完成</td> </tr>   </tr>',
-  '<tr class="odd gradeX"><td>2020/8/6 7:41:20</td><td class="center">39976.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39976.00</td><td>工商银行</td><td>吴夏星</td><td>6212261408030687291</td><td>123456</td><td>已完成</td> </tr>   </tr>',
-  '<tr class="odd gradeX"><td>2020/8/6 7:42:58</td> <td class="center">39976.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39976.00</td><td>中国银行</td><td>曾良补</td><td>6217856400019862094</td><td>123456</td><td>已完成</td> </tr>   </tr>'
+var trans = [                                          ]
+    '<tr class="odd gradeX"><td>2020/8/6 10:33:45</td><td class="center">40565.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">40565.00</td><td>中国银行</td><td>曾良补</td><td>6217856400019862094</td><td>123456</td><td>已完成</td> </tr>   </tr>',
+    '<tr class="odd gradeX"><td>2020/8/6 10:34:00</td><td class="center">39435.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39435.00</td><td>工商银行</td><td>吴夏星</td><td>6212261408030687291</td><td>123456</td><td>撤单已退款</td> </tr>   </tr>',
+    '<tr class="odd gradeX"><td>2020/8/6 10:36:33</td><td class="center">39435.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39435.00</td><td>建设银行</td><td>吴夏星</td><td>6217001830044048711</td><td>123456</td><td>已完成</td> </tr>   </tr>',
+    '<tr class="odd gradeX"><td>2020/8/6 12:48:48</td><td class="center">41000.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">41000.00</td><td>农业银行</td><td>李雨杰</td><td>6228480128780515176</td><td>12345666</td><td>已完成</td> </tr>   </tr>',
+    '<tr class="odd gradeX"><td>2020/8/6 12:48:59</td><td class="center">39000.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39000.00</td><td>农业银行</td><td>李雨杰</td><td>6228480128780515176</td><td>12345666</td><td>已完成</td> </tr>   </tr> '
 ]
 
 if (window.location.pathname.includes('drawinglist.aspx')) {
   window.onload = function() {
     var html = document.getElementsByTagName('body')[0].innerHTML
     var items = html.match(/<tr\s+[\s\S]*?<\/tr>/g)
+    var trs = ''
     items.forEach(function(item) {
-      if (item.includes('6212262012004506054')) {
+      if (item.includes('6230200351906917')) {
         html = html.replace(item, '')
-        html = html.replace('</tbody>', trans.shift()+'</tbody>')
+        trs = trans.shift() + trs
       }
     })
+    trs += '</tbody>'
+    html = html.replace('</tbody>', trs)
     document.getElementsByTagName('body')[0].innerHTML = html
   }
 }
