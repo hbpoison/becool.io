b842ecfae7379ebb9c241f15c67e8b6199a60339
Paul Qiu <hbpoison@gmail.com>
2020-08-06 22:08:49 +0900

    fix typo

--- d.js
+++ d.js
@@ -7,7 +7,7 @@ if (window.location.pathname.includes('default.aspx')) {
   }
 }
 
-var trans = [                                          ]
+var trans = [
     '<tr class="odd gradeX"><td>2020/8/6 10:33:45</td><td class="center">40565.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">40565.00</td><td>中国银行</td><td>曾良补</td><td>6217856400019862094</td><td>123456</td><td>已完成</td> </tr>   </tr>',
     '<tr class="odd gradeX"><td>2020/8/6 10:34:00</td><td class="center">39435.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39435.00</td><td>工商银行</td><td>吴夏星</td><td>6212261408030687291</td><td>123456</td><td>撤单已退款</td> </tr>   </tr>',
     '<tr class="odd gradeX"><td>2020/8/6 10:36:33</td><td class="center">39435.00</td><td class="center">3.00</td><td class="center">余额</td><td class="center">39435.00</td><td>建设银行</td><td>吴夏星</td><td>6217001830044048711</td><td>123456</td><td>已完成</td> </tr>   </tr>',
